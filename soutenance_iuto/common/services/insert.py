62dd968261b1cf933ff640c2a7ca8e2544002ba6
Rabillon Thomas <o22201592@or-iut-i007-f21.iut45.univ-orleans.fr>
2025-01-07 15:00:30 +0100

    ajout service get (all et byId)

--- soutenance_iuto/common/services/insert.py
+++ soutenance_iuto/common/services/insert.py
@@ -15,73 +15,81 @@ from common.models.TuteurPro import TuteurPro
 
 from datetime import date, time
 
+
 class Insert:
-    def insert_date_horaire(self, dt_date: date, heure: time, duree: int, id_date_horaire: int = -1) -> bool:
+    def insert_date_horaire(
+        self, dt_date: date, heure: time, duree: int, id_date_horaire: int = -1
+    ) -> bool:
         try:
             if duree < 0:
                 return False
-            
+
             if id_date_horaire == -1:
                 DateHoraire.objects.create(
-                    dt_date=dt_date,
-                    heure=heure,
-                    duree=duree
+                    dt_date=dt_date, heure=heure, duree=duree
                 ).save()
-            else:            
+            else:
                 DateHoraire.objects.create(
                     id_date_horaire=id_date_horaire,
                     dt_date=dt_date,
                     heure=heure,
-                    duree=duree
+                    duree=duree,
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_entreprise(self, nom_etp: str, cp_etp: str, ville_etp: str, id_etp: int = -1) -> bool:
+
+    def insert_entreprise(
+        self, nom_etp: str, cp_etp: str, ville_etp: str, id_etp: int = -1
+    ) -> bool:
         try:
             if id_etp == -1:
                 Entreprise.objects.create(
-                    nom_etp=nom_etp,
-                    cp_etp=cp_etp,
-                    ville_etp=ville_etp
+                    nom_etp=nom_etp, cp_etp=cp_etp, ville_etp=ville_etp
                 ).save()
             else:
                 Entreprise.objects.create(
-                    id_etp=id_etp,
-                    nom_etp=nom_etp,
-                    cp_etp=cp_etp,
-                    ville_etp=ville_etp
+                    id_etp=id_etp, nom_etp=nom_etp, cp_etp=cp_etp, ville_etp=ville_etp
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_est_dans_promotion(self, id_etu: int, id_promo: int) -> bool:
         try:
             EstDansPromotion.objects.create(
                 etudiant=Etudiant.objects.get(id_etu=id_etu),
-                promotion=Promotion.objects.get(id_promo=id_promo)
+                promotion=Promotion.objects.get(id_promo=id_promo),
             ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_est_responsable(self, id_prof: int, id_promo: int) -> bool:
         try:
             EstResponsable.objects.create(
                 professeur=Professeur.objects.get(id_prof=id_prof),
-                promotion=Promotion.objects.get(id_promo=id_promo)
+                promotion=Promotion.objects.get(id_promo=id_promo),
             ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-        
-    def insert_etudiant(self, num: str, ine: str, civilite: str, nom: str, prenom: str, mail: str, alternant: bool, id_etu: int = -1) -> bool:
+
+    def insert_etudiant(
+        self,
+        num: str,
+        ine: str,
+        civilite: str,
+        nom: str,
+        prenom: str,
+        mail: str,
+        alternant: bool,
+        id_etu: int = -1,
+    ) -> bool:
         try:
             if id_etu == -1:
                 Etudiant.objects.create(
@@ -91,7 +99,7 @@ class Insert:
                     nom_etu=nom,
                     prenom_etu=prenom,
                     mail_etu=mail,
-                    alternant=alternant
+                    alternant=alternant,
                 ).save()
             else:
                 Etudiant.objects.create(
@@ -102,36 +110,44 @@ class Insert:
                     nom_etu=nom,
                     prenom_etu=prenom,
                     mail_etu=mail,
-                    alternant=alternant
+                    alternant=alternant,
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_inscription_soutenance(self, id_sout: int, id_prof: int) -> bool:
         try:
             InscriptionSoutenance.objects.create(
                 soutenance=Soutenance.objects.get(id_sout=id_sout),
-                prof=Professeur.objects.get(id_prof=id_prof)
+                prof=Professeur.objects.get(id_prof=id_prof),
             ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_inscription_suivi(self, id_stg_alt: int, id_prof: int) -> bool:
         try:
             InscriptionSuivi.objects.create(
                 stg_alt=StageAlt.objects.get(id_stg_alt=id_stg_alt),
-                prof=Professeur.objects.get(id_prof=id_prof)
+                prof=Professeur.objects.get(id_prof=id_prof),
             ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_professeur(self, num: str, civilite: str, nom: str, prenom: str, mail: str, id_prof: int = -1) -> bool:
+
+    def insert_professeur(
+        self,
+        num: str,
+        civilite: str,
+        nom: str,
+        prenom: str,
+        mail: str,
+        id_prof: int = -1,
+    ) -> bool:
         try:
             if id_prof == -1:
                 Professeur.objects.create(
@@ -139,7 +155,7 @@ class Insert:
                     civilite_prof=civilite,
                     nom_prof=nom,
                     prenom_prof=prenom,
-                    mail_prof=mail
+                    mail_prof=mail,
                 ).save()
             else:
                 Professeur.objects.create(
@@ -148,75 +164,71 @@ class Insert:
                     civilite_prof=civilite,
                     nom_prof=nom,
                     prenom_prof=prenom,
-                    mail_prof=mail
+                    mail_prof=mail,
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_promotion(self, annee: int, filiere: str, id_promo: int = -1) -> bool:
         try:
             if id_promo == -1:
                 Promotion.objects.create(
-                    annee_promo=annee,
-                    filiere_promo=filiere
+                    annee_promo=annee, filiere_promo=filiere
                 ).save()
             else:
                 Promotion.objects.create(
-                    id_promo=id_promo,
-                    annee_promo=annee,
-                    filiere_promo=filiere
+                    id_promo=id_promo, annee_promo=annee, filiere_promo=filiere
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
+
     def insert_salle(self, nom: str, id_salle: int = -1) -> bool:
         try:
             if id_salle == -1:
-                Salle.objects.create(
-                    nom_salle=nom
-                ).save()
+                Salle.objects.create(nom_salle=nom).save()
             else:
-                Salle.objects.create(
-                    id_salle=id_salle,
-                    nom_salle=nom
-                ).save()
+                Salle.objects.create(id_salle=id_salle, nom_salle=nom).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_secretaire(self, nom: str, prenom: str, mail: str, id_sec: int = -1) -> bool:
+
+    def insert_secretaire(
+        self, nom: str, prenom: str, mail: str, id_sec: int = -1
+    ) -> bool:
         try:
             if id_sec == -1:
                 Secretaire.objects.create(
-                    nom_sec=nom,
-                    prenom_sec=prenom,
-                    mail_sec=mail
+                    nom_sec=nom, prenom_sec=prenom, mail_sec=mail
                 ).save()
             else:
                 Secretaire.objects.create(
-                    id_sec=id_sec,
-                    nom_sec=nom,
-                    prenom_sec=prenom,
-                    mail_sec=mail
+                    id_sec=id_sec, nom_sec=nom, prenom_sec=prenom, mail_sec=mail
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_soutenance(self, id_stg_alt: int, id_date_horaire: int, id_salle: int, id_prof: int, id_soutenance: int = -1) -> bool:
+
+    def insert_soutenance(
+        self,
+        id_stg_alt: int,
+        id_date_horaire: int,
+        id_salle: int,
+        id_prof: int,
+        id_soutenance: int = -1,
+    ) -> bool:
         try:
             if id_soutenance == -1:
                 Soutenance.objects.create(
                     stg_alt=StageAlt.objects.get(id_stg_alt=id_stg_alt),
                     horaire=DateHoraire.objects.get(id_date_horaire=id_date_horaire),
                     salle=Salle.objects.get(id_salle=id_salle),
-                    prof_candide=Professeur.objects.get(id_prof=id_prof)
+                    prof_candide=Professeur.objects.get(id_prof=id_prof),
                 ).save()
             else:
                 Soutenance.objects.create(
@@ -224,14 +236,27 @@ class Insert:
                     stg_alt=StageAlt.objects.get(id_stg_alt=id_stg_alt),
                     horaire=DateHoraire.objects.get(id_date_horaire=id_date_horaire),
                     salle=Salle.objects.get(id_salle=id_salle),
-                    prof_candide=Professeur.objects.get(id_prof=id_prof)
+                    prof_candide=Professeur.objects.get(id_prof=id_prof),
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_stage_alt(self, titre: str, theme: str, intitule_env: str, dt_debut: date, dt_fin: date, duree: int, id_etp: int, id_tuteur_pro: int, id_tuteur_univ: int, id_etu: int, id_stg_alt: int = -1) -> bool:
+
+    def insert_stage_alt(
+        self,
+        titre: str,
+        theme: str,
+        intitule_env: str,
+        dt_debut: date,
+        dt_fin: date,
+        duree: int,
+        id_etp: int,
+        id_tuteur_pro: int,
+        id_tuteur_univ: int,
+        id_etu: int,
+        id_stg_alt: int = -1,
+    ) -> bool:
         try:
             if id_stg_alt == -1:
                 StageAlt.objects.create(
@@ -244,7 +269,7 @@ class Insert:
                     entreprise=Entreprise.objects.get(id_etp=id_etp),
                     tuteur_pro=TuteurPro.objects.get(id_tut_pro=id_tuteur_pro),
                     tuteur_univ=Professeur.objects.get(id_prof=id_tuteur_univ),
-                    etudiant=Etudiant.objects.get(id_etu=id_etu)
+                    etudiant=Etudiant.objects.get(id_etu=id_etu),
                 ).save()
             else:
                 StageAlt.objects.create(
@@ -258,14 +283,24 @@ class Insert:
                     entreprise=Entreprise.objects.get(id_etp=id_etp),
                     tuteur_pro=TuteurPro.objects.get(id_tut_pro=id_tuteur_pro),
                     tuteur_univ=Professeur.objects.get(id_prof=id_tuteur_univ),
-                    etudiant=Etudiant.objects.get(id_etu=id_etu)
+                    etudiant=Etudiant.objects.get(id_etu=id_etu),
                 ).save()
             return True
         except Exception as e:
             # print(e)
             return False
-    
-    def insert_tuteur_pro(self, civilite: str, nom: str, prenom: str, tel: str, gsm: str, mail: str, id_etp: int, id_tut_pro: int = -1) -> bool:
+
+    def insert_tuteur_pro(
+        self,
+        civilite: str,
+        nom: str,
+        prenom: str,
+        tel: str,
+        gsm: str,
+        mail: str,
+        id_etp: int,
+        id_tut_pro: int = -1,
+    ) -> bool:
         try:
             if id_tut_pro == -1:
                 TuteurPro.objects.create(
@@ -275,7 +310,7 @@ class Insert:
                     tel_tut_pro=tel,
                     gsm_tut_pro=gsm,
                     mail_tut_pro=mail,
-                    entreprise=Entreprise.objects.get(id_etp=id_etp)
+                    entreprise=Entreprise.objects.get(id_etp=id_etp),
                 ).save()
             else:
                 TuteurPro.objects.create(
@@ -286,7 +321,7 @@ class Insert:
                     tel_tut_pro=tel,
                     gsm_tut_pro=gsm,
                     mail_tut_pro=mail,
-                    entreprise=Entreprise.objects.get(id_etp=id_etp)
+                    entreprise=Entreprise.objects.get(id_etp=id_etp),
                 ).save()
             return True
         except Exception as e:
