be340466e2d4e2dcf4dde3915cc9c6f751e6db6f
Evann45 <evann45yang@mail.com>
2025-01-11 17:36:33 +0100

    A jour

--- soutenance_iuto/app_entreprise/urls.py
+++ soutenance_iuto/app_entreprise/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('InfoEtudiant', views.InfoEtudiantView.as_view(), name='info_etu_entreprise'),
-    path('home/', views.HomeView.as_view(), name='home_entreprise'),
-    path('', views.HomeView.as_view(), name='home_entreprise'),
+    path('home/', views.HomeView.as_view(), name='tuteur_pro_home'),
+    path('', views.HomeView.as_view(), name='tuteur_pro_home'),
 ]
--- soutenance_iuto/app_entreprise/views.py
+++ soutenance_iuto/app_entreprise/views.py
@@ -2,26 +2,6 @@ from django.views.generic import TemplateView
 from common.services.get import Get, GetById
 from django.shortcuts import redirect
 
-
-class HomeView(TemplateView):
-    template_name = 'app_entreprise/home.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['titre'] = "Accueil"
-        user = get_user(self.request.COOKIES['user_data'])
-        context['user'] = user
-        return context
-
-class InfoEtudiantView(TemplateView):
-    template_name = 'app_entreprise/info_etudiant.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super(InfoEtudiantView, self).get_context_data(**kwargs)
-        context['titre'] = "Informations sur l'étudiant"
-        context['etudiant'] = Get.get_etudiant_by_tuteur_pro_id(1)
-        return context
-    
 def get_user(cookie):
     id_user = cookie.split(":")[0]
     type_user = cookie.split(":")[1]
@@ -35,3 +15,37 @@ def get_user(cookie):
         return redirect("professeur_home")
     else:
         return redirect("login_common")
+    
+def redirect_user(cookie):
+    if cookie is None:
+        return redirect("login_common")
+    type_user = cookie.split(":")[1]
+    if type_user == "etudiant":
+        return redirect("etudiant_home")
+    elif type_user == "tuteur_pro":
+        return redirect("tuteur_pro_home")
+    elif type_user == "professeur":
+        return redirect("professeur_home")
+
+class HomeView(TemplateView):
+    template_name = 'app_entreprise/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titre'] = "Accueil"
+        user = get_user(self.request.COOKIES['user_data'])
+        context['user'] = user
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        return redirect_user(request.COOKIES['user_data'])
+
+class InfoEtudiantView(TemplateView):
+    template_name = 'app_entreprise/info_etudiant.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(InfoEtudiantView, self).get_context_data(**kwargs)
+        context['titre'] = "Informations sur l'étudiant"
+        context['etudiant'] = Get.get_etudiant_by_tuteur_pro_id(1)
+        return context
+    
